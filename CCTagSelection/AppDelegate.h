1bfac026fc23d0618554b552f48840a053cc1de9
chenwei <chenwei@guanfu.cn>
2017-06-01 17:00:28 +0800

    V1

---
+++ CCTagSelection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCTagSelection
+//
+//  Created by YSCC on 2017/6/1.
+//  Copyright © 2017年 YSCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCTagSelection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCTagSelection
+//
+//  Created by YSCC on 2017/6/1.
+//  Copyright © 2017年 YSCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
